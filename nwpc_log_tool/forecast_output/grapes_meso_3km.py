530dceca8b01c36ebbb9ba745e25b9a49428bb45
perillaroc <perillaroc@gmail.com>
2020-05-04 09:17:34 +0800

    :sparkles: add fcst parser for grapes_meso_3km

---
+++ nwpc_log_tool/forecast_output/grapes_meso_3km.py
@@ -0,0 +1,24 @@
+import re
+
+import pandas as pd
+
+
+def get_step_time_from_file(file_path):
+    p = re.compile(r"Timing for processing for step\s+(.+) \((.*)\):\s+(.+) elapsed seconds\.")
+    data = []
+    index = []
+    with open(file_path) as f:
+        for line in f:
+            m = p.match(line)
+            if m is None:
+                continue
+            step = int(m.group(1))
+            valid_time = pd.to_datetime(m.group(2), format='%Y%m%d%H:%M:%S')
+            time = float(m.group(3))
+            data.append({
+                "valid_time": valid_time,
+                "time": time
+            })
+            index.append(step)
+    df = pd.DataFrame(data, index=index)
+    return df
