f9e20da45240f5b7febdc894dbbd966c8fddf49c
perillaroc <perillaroc@gmail.com>
2020-05-09 14:58:14 +0800

    :bulb: add docstrings for functions.

--- nwpc_log_tool/forecast_output/grapes_gfs.py
+++ nwpc_log_tool/forecast_output/grapes_gfs.py
@@ -1,9 +1,34 @@
 import re
+from pathlib import Path
 
 import pandas as pd
 
 
-def get_step_time_from_file(file_path):
+def get_step_time_from_file(file_path: str or Path):
+    """
+    Get seconds for each step from std.out.0000 of GRAPES GFS.
+
+    Example output:
+
+     begin operational forecast----
+     begin of gcr  5.298061077439735E-005
+     RES of gcr  1.062585033967659E-023 in           37 iterations
+    Timing for processing for step        1:   15.51460 elapsed seconds.
+     begin of gcr  6.001155404370011E-005
+     RES of gcr  1.006302069134485E-023 in           37 iterations
+    Timing for processing for step        2:    0.30060 elapsed seconds.
+     begin of gcr  5.512293401798762E-005
+     RES of gcr  1.112349462442307E-023 in           37 iterations
+
+    Parameters
+    ----------
+    file_path: str or Path
+
+    Returns
+    -------
+    pandas.DataFrame:
+        table data with "valid_time" and "time" as columns, and step number as index.
+    """
     p = re.compile(r"Timing for processing for step\s+(.+):\s+(.+) elapsed seconds\.")
     data = []
     index = []
--- nwpc_log_tool/forecast_output/grapes_meso_3km.py
+++ nwpc_log_tool/forecast_output/grapes_meso_3km.py
@@ -1,9 +1,34 @@
 import re
+from pathlib import Path
 
 import pandas as pd
 
 
-def get_step_time_from_file(file_path):
+def get_step_time_from_file(file_path: str or Path) -> pd.DataFrame:
+    """
+    Get seconds for each step from ecflow job output fcst.1 of GRAPES MESO 3KM.
+
+    Example output:
+
+    Timing for processing for step 1 (2020050900:00:00):         10.60030 elapsed seconds.
+    Timing for processing for step 1 (2020050900:00:00):          8.34263 cpu seconds.
+     begin of gcr  3.311504627275603E-004
+     RES of gcr  7.923976079163864E-013 in           37 iterations
+     warm start: grid%do_cld = T
+    Timing for processing for step 2 (2020050900:00:30):          0.73180 elapsed seconds.
+    Timing for processing for step 2 (2020050900:00:30):          0.73085 cpu seconds.
+     begin of gcr  1.786058322574234E-004
+     RES of gcr  9.312605195708282E-013 in           36 iterations
+
+    Parameters
+    ----------
+    file_path: str or Path
+
+    Returns
+    -------
+    pandas.DataFrame:
+        table data with "valid_time" and "time" as columns, and step number as index.
+    """
     p = re.compile(r"Timing for processing for step\s+(.+) \((.*)\):\s+(.+) elapsed seconds\.")
     data = []
     index = []
@@ -24,7 +49,30 @@ def get_step_time_from_file(file_path):
     return df
 
 
-def get_output_time_from_file(file_path):
+def get_output_time_from_file(file_path: str or Path) -> pd.DataFrame:
+    """
+    Get seconds for modelvar output from ecflow job output fcst.1 of GRAPES MESO 3KM.
+
+    Example output:
+
+    Timing for processing for step 129 (2020050900:59:30):          0.77260 elapsed seconds.
+    Timing for processing for step 129 (2020050900:59:30):          0.77182 cpu seconds.
+     output modelvar use    2.41637611389160      seconds
+      post grib2 compress and output use   0.456164121627808       seconds.
+     ADJUST TIME STEP: old dt=   23.00000      new dt=   24.00000      MaxCfl=
+       1.155885
+     begin of gcr  8.673518207560389E-006
+     RES of gcr  9.119498186871097E-013 in           24 iterations
+
+    Parameters
+    ----------
+    file_path: str or Path
+
+    Returns
+    -------
+    pandas.DataFrame:
+        table data with "time" as column.
+    """
     p = re.compile(r"output modelvar use\s+([0-9.]*)\s+seconds")
     data = []
     with open(file_path) as f:
