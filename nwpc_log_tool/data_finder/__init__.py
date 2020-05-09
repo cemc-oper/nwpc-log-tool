9528d5b31a1c3176cb8cf912c6f4d8533cb18677
perillaroc <perillaroc@gmail.com>
2020-05-09 15:16:11 +0800

    :bulb: add docstrings for data_finder

--- nwpc_log_tool/data_finder/__init__.py
+++ nwpc_log_tool/data_finder/__init__.py
@@ -8,6 +8,34 @@ def find_local_file(
         config_dir: str or Path or None = None,
         **kwargs,
 ) -> Path or None:
+    """
+    Find files using ``nwpc_data.data_finder.find_local_file`` with config files under conf directory.
+
+    Parameters
+    ----------
+    data_type: str
+        data type, such as "grapes_gfs_gmf/log/fcst_long_std_out"
+    config_dir: str or Path or None
+        default is None, using conf directory.
+    kwargs:
+        other parameters using by ``nwpc_data.data_finder.find_local_file``
+
+    Returns
+    -------
+    Path or None:
+        data file path if found or None if not.
+
+    Examples
+    --------
+    Find std out file for GRAPES GFS GMF.
+
+    >>> find_local_file(
+    ...     "grapes_gfs_gmf/log/fcst_long_std_out",
+    ...     config_dir="2020050500",
+    ... )
+    PosixPath('/g1/COMMONDATA/OPER/NWPC/GRAPES_GFS_GMF/Log/2020050421/std.out_fcst_2020050500')
+
+    """
     if config_dir is None:
         config_dir = _get_default_local_config_path()
     return _find_local_file(
